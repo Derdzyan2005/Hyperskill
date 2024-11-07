6eae5036912524d4453ec5d4e6268831dffbb8e1
Derdzyan2005 <a.derdzyan2005@gmail.com>
2024-11-07 22:27:53 +0400

    Create stage1.java

---
+++ Java/FirstProject/stage1.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Prices:\n" +
+            "Bubblegum: $2\n" +
+            "Toffee: $0.2\n" +
+            "Ice cream: $5\n" +
+            "Milk chocolate: $4\n" +
+            "Doughnut: $2.5\n" +
+            "Pancake: $3.2");       
+  }
+}
