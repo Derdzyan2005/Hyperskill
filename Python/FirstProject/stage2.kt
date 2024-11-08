b36a61737a0a6c1ee3efc8927ab018cc4bc1850c
Derdzyan2005 <a.derdzyan2005@gmail.com>
2024-11-08 17:43:23 +0400

    Create stage2.kt

---
+++ Python/FirstProject/stage2.kt
@@ -0,0 +1,11 @@
+print("""Earned amount:
+Bubblegum: $202
+Toffee: $118
+Ice cream: $2250
+Milk chocolate: $1680
+Doughnut: $1075
+Pancake: $80""")
+
+total_sum = 202 + 118 + 2250 + 1680 + 1075 + 80
+
+print(total_sum)
